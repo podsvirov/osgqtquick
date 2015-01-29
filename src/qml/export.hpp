161f099e9c96372924c92e2474913095612beeba
Konstantin Podsvirov <konstantin@podsvirov.pro>
2015-01-29 19:13:33 +0300

    Fix OSGQTQML_EXPORT for UNIX systems

--- src/qml/export.hpp
+++ src/qml/export.hpp
@@ -2,15 +2,15 @@
 #define _OSGQTQML_EXPORT_ 1
 
 #if defined(_MSC_VER) || defined(__CYGWIN__) || defined(__MINGW32__) || defined( __BCPLUSPLUS__) || defined( __MWERKS__)
-    #  if defined( OSG_LIBRARY_STATIC )
-        #    define OSGQTQML_EXPORT
-        #  elif defined( OSGQTQML_LIBRARY )
-        #    define OSGQTQML_EXPORT   __declspec(dllexport)
-        #  else
-        #    define OSGQTQML_EXPORT   __declspec(dllimport)
-        #endif
+#  if defined( OSG_LIBRARY_STATIC )
+#    define OSGQTQML_EXPORT
+#  elif defined( OSGQTQML_LIBRARY )
+#    define OSGQTQML_EXPORT __declspec(dllexport)
+#  else
+#    define OSGQTQML_EXPORT __declspec(dllimport)
+#  endif
 #else
-        #define OSGQT_EXPORT
+#  define OSGQTQML_EXPORT
 #endif
 
 #endif // _OSGQTQML_EXPORT_
